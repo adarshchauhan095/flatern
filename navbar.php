fd878b3cb47e8c2825da18c64f14ab829b90ade6
adarshchauhan095 <akchauhan095@gmail.com>
2021-03-04 12:29:36 +0530

    html-to-php & all files are succes

---
+++ navbar.php
@@ -0,0 +1,60 @@
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i><a href="mailto:akchauhan095@gmail.com">akchauhan095@gmail.com</a>
+        <i class="icofont-phone"></i><a href="tel:+917015027886">+91 70150 27886</a>
+      </div>
+      <div class="social-links">
+        <a target="_blank" href="https://twitter.com/akchauhan095" class="twitter"><i class="icofont-twitter"></i></a>
+        <a target="_blank" href="https://www.facebook.com/akchauhan095" class="facebook"><i class="icofont-facebook"></i></a>
+        <a target="_blank" href="https://www.instagram.com/chauhan_adarsh_/" class="instagram"><i class="icofont-instagram"></i></a>
+        <!-- <a target="_blank" href="#" class="skype"><i class="icofont-skype"></i></a> -->
+        <a target="_blank" href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.php">Flattern</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="services.php">Services</a></li>
+          <li><a href="portfolio.php">Portfolio</a></li>
+          <li><a href="testimonials.php">Testimonials</a></li>
+          <li><a href="pricing.php">Pricing</a></li>
+          <li><a href="blog.php">Blog</a></li>
+          <!-- <li class="drop-down"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="drop-down"><a href="#">Drop Down 2</a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li> -->
+          <li><a href="contact.php">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
