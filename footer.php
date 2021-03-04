de453e31fbed6eeabe225d4234943e0dd627c560
adarshchauhan095 <akchauhan095@gmail.com>
2021-03-04 13:09:38 +0530

    footer modified

--- footer.php
+++ footer.php
@@ -6,22 +6,32 @@
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Flattern</h3>
+            <h3>BracketFly</h3>
             <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              Model Town <br>
+              Karnal (132001)<br>
+              Haryana, India <br><br>
+              <table>
+                <tr>
+                <td><strong>Phone:</strong></td>
+                <td>+91 70150 27886</td>
+                </tr>
+                <tr>
+                <td></td>
+                <td>+91 89509 85808</td>
+                </tr>
+              </table>
+              <!-- <strong>Phone:</strong><br> +91 70150 27886,<br> +91 89509 85808<br> -->
+              <strong>Email:</strong> info@bracketfly.com<br>
             </p>
           </div>
 
           <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
+            <h4>Quick Links</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Services</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
               <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
             </ul>
@@ -30,17 +40,17 @@
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Graphic Design</a></li>
             </ul>
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <h4>Subscribe to our Newsletter</h4>
+            <p>Stay current with our latest insights</p>
             <form action="" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form>
@@ -54,22 +64,18 @@
 
       <div class="mr-md-auto text-center text-md-left">
         <div class="copyright">
-          &copy; Copyright <strong><span>Flattern</span></strong>. All Rights Reserved
+          &copy; Copyright <strong><span>BracketFly</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flattern-multipurpose-bootstrap-template/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          Developed by <a href="https://bracketfly.com/">BracketFly</a>
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a target="_blank" href="https://twitter.com/akchauhan095" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a target="_blank" href="https://www.facebook.com/akchauhan095" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a target="_blank" href="https://www.instagram.com/chauhan_adarsh_/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <!-- <a target="_blank" href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+        <a target="_blank" href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </footer><!-- End Footer -->
